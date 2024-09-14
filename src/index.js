c8358d8a66bbb4ea5096598fe0325b9814d72354
Mutyamjai <22ucs130@lnmiit.ac.in>
2024-09-15 02:04:40 +0530

    models created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
-
+import express from "express";
+const app = express()
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() =>{
+    
+    app.listen(process.env.PORT || 4000 ,() => {
+        console.log(`server is running at ${process.env.PORT}`)
+    })
+})
+.catch((e) => {
+    console.log("MONGO DB Connection failed",e)
+})
+
+process.on('uncaughtException', (err) => {
+    console.error("There was an uncaught exception: ", err);
+});
+
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error("Unhandled Rejection at: ", promise, " reason: ", reason);
+});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+export {asyncHandler}
+
+const asyncHandler = (fn) => async (req,res,next) => {
+    try{
+        await fn(req,res,next);
+    }
+    catch(e){
+        res.status(e.code || 500).json({
+            success: false,
+            message: e.message
+        })
+    }
+}
